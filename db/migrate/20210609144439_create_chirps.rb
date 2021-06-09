567d1ee38359be86d56ca1cbf7e76de800a1d0f4
Jeffrey Lu <hi@jeffreyclu.com>
2021-06-09 10:46:40 -0400

    disallow nulls

--- db/migrate/20210609144439_create_chirps.rb
+++ db/migrate/20210609144439_create_chirps.rb
@@ -1,7 +1,7 @@
 class CreateChirps < ActiveRecord::Migration[6.1]
   def change
     create_table :chirps do |t|
-      t.string :chirp
+      t.string :chirp, null: false
 
       t.timestamps
     end
